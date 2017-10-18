c83832eb5dfcb194587203bea70950caaaa67663
beasta <barryblaha@gmail.com>
2017-10-18 02:04:51 -0700

    update command structure

--- commands/!wallet.js
+++ commands/!wallet.js
@@ -2,27 +2,29 @@ var _ = require("lodash");
 var cheerio = require("cheerio");
 var request = require("request");
 var util 	= require('../util');
+var fs = require('fs');
+var path = require('path');
 var info = "";
 var result = [];
-var scrapeEtherscanForCurrentTotal = (totalSent, callBack, channel) => {
-  request("https://etherscan.io/address/0x13f11c9905a08ca76e3e853be63d4f0944326c72", function (error, response, html) {
+var etherscanToken = JSON.parse(fs.readFileSync(path.basename('keys.json'))).etherscanToken;
+const contractAddress = '0x13f11c9905a08ca76e3e853be63d4f0944326c72';
 
+var scrapeEtherscanForCurrentTotal = (totalSent, callBack, channel) => {
+  request(`https://etherscan.io/address/${contractAddress}`, function (error, response, html) {
     var $ = cheerio.load(html);
     var re = /\d*\.\d*/;
     var currentWallet;
     var totalRaised;
-
+    const url = `https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${etherscanToken}`;
 
     $("#ContentPlaceHolder1_divSummary div table tbody tr td:nth-child(2)").each(function (i, element) {
       result.push($(element).text());
-      console.log('result', result);
     });
 
     currentWallet = _.round(re.exec(result[0]), 2);
-    totalRaised = currentWallet + totalSent;
+    totalRaised = _.round(currentWallet + totalSent, 2);
 
-
-    request('https://api.etherscan.io/api?module=stats&action=ethprice&apikey=7NU4FJ7QBV34YA8A662KRBZPHRCBETVNPMA', (err, res, body) => {
+    request(url, (err, res, body) => {
       var ethusd = JSON.parse(res.body).result.ethusd;
       var totalRaisedUsd = _.round((totalRaised* ethusd), 2);
       info =
@@ -36,7 +38,6 @@ var scrapeEtherscanForCurrentTotal = (totalSent, callBack, channel) => {
           ${_.trim(result[2], '\n')}` +
           `\nThe current ethereum price from etherscan is:
           ${ethusd} ETH/USD`
-      console.log('result', result);
       util.postMessage(channel, info, true);
     })
   });
